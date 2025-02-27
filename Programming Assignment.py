855f497df2d8a9124da9a96cdd014fe5870036b2
Awais21 <mawaisx.667@gmail.com>
2025-02-27 11:57:47 +0500

    Assignment pg71

---
+++ Programming Assignment.py
@@ -0,0 +1,25 @@
+#display serval numbers
+print(31,9,76,45,98)
+
+#Solve and show the summation of 200+12
+print("200+12=", 200+12)
+
+#solve and show summation using variables
+x=64
+y=21
+sum=x+y
+print("sum=",sum)
+
+#print the word 'Lucky' inside a variable
+s="Lucky"
+print(s)
+
+#print today's date in the format "today is 2/2/2016"
+day=2
+month=2
+year=2016
+print("Today is", day,"/",month,"/",year)
+
+#ask user for phone number and display it
+phone=input("Enter your phone number")
+print("Your Number is",phone)
